834509b91d94102b30ec122df695c7b825a81a26
Lucas Almeida <39913588+lucas-almeida026@users.noreply.github.com>
2021-11-20 09:56:18 -0300

    org

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,5 @@
 export * from './main'
+export * from './mui'
 export * from './types'
+export * from './others'
+export * from './promise'
--- src/main/index.ts
+++ src/main/index.ts
@@ -1,14 +1,2 @@
-//promises
-export { handlePromise, handlePromiseChain } from './promise'
-
 //general
 export { exists, orderArrayOfObjects, removeEmptyAttributes } from './general'
-
-//mui
-export { flexColumn, flexRow, stackFlexColumn, stackFlexRow} from '../mui'
-
-//types
-export { Merge } from '../types'
-
-//others
-export { withLanguages } from './others'
