1b09d54c292f7ad7bf3baaf5d9d48a806a85cc33
Lucas Almeida <39913588+lucas-almeida026@users.noreply.github.com>
2021-12-08 07:20:08 -0300

    lib build

--- lib/promise/index.d.ts
+++ lib/promise/index.d.ts
@@ -1,3 +1,3 @@
-export declare const handlePromise: (p: Promise<any>) => Promise<[any | null, any | null]>;
+export declare function handlePromise<ResultType>(p: Promise<ResultType>, callback?: Function): Promise<[ResultType | null, Error | null]>;
 export declare const handlePromiseChain: (...p: Promise<any>[]) => Promise<[any, any]>;
 //# sourceMappingURL=index.d.ts.map
